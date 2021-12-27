872f0323615144d6212d83c98fc6457da9c31406
Nishant Anil Rajput <nishantrajputa11@gmail.com>
2021-12-27 11:15:35 +0530

    video 48 about jwt token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = "SayLalisaloveMe";
 
 //create a user using: POST  "/api/auth/createuser" : no login require auth
 
@@ -24,17 +28,26 @@ router.post('/createuser', [
         if (user) {
             return res.status(400).json({ error: "Sorry a user with this email already exist" })
         }
+
+        const salt = await bcrypt.genSalt(10);
+        SecPass = await bcrypt.hash(req.body.password, salt);
         // create a new user
         user = await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password,
+            password: SecPass,
         })
-        res.json(user)
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+        const authToken = jwt.sign(data, JWT_SECRET);
+        res.json({ authToken });
     }
-    catch(error){
+    catch (error) {
         console.error(error.message);
-        res.status(500).json({error:"Some error occured"})
+        res.status(500).json({ error: "Some error occured" })
     }
 })
 module.exports = router;
